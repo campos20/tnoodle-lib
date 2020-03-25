6514a8d8d5908451efd40f989990a63f18f90023
Alexandre Campos <camposalexandreh@gmail.com>
2020-03-24 22:53:52 -0300

    Add lombok to Element

--- svglite/src/main/java/org/worldcubeassociation/tnoodle/svglite/Element.java
+++ svglite/src/main/java/org/worldcubeassociation/tnoodle/svglite/Element.java
@@ -3,175 +3,168 @@ package org.worldcubeassociation.tnoodle.svglite;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Element {
 
-    protected String tag;
-    protected HashMap<String, String> attributes;
-    protected HashMap<String, String> style;
-    protected ArrayList<Element> children;
-    protected String content;
-    public Element(String tag) {
-        this.tag = tag;
-        this.children = new ArrayList<Element>();
-        this.attributes = new HashMap<String, String>();
-        this.style = new HashMap<String, String>();
-        this.content = null;
-    }
+	protected String tag;
 
-    public Element(Element e) {
-        this.tag = e.tag;
-        this.attributes = new HashMap<String, String>(e.attributes);
-        this.style = new HashMap<String, String>(e.style);
-        this.children = e.copyChildren();
-        this.content = content;
-    }
+	@Getter
+	protected HashMap<String, String> attributes;
 
-    protected ArrayList<Element> copyChildren() {
-        ArrayList<Element> childrenCopy = new ArrayList<Element>();
-        for(Element child : children) {
-            childrenCopy.add(new Element(child));
-        }
-        return childrenCopy;
-    }
+	@Getter
+	protected HashMap<String, String> style;
 
-    public String getContent() {
-        return content;
-    }
+	@Getter
+	protected ArrayList<Element> children;
 
-    public void setContent(String content) {
-        this.content = content;
-    }
+	@Getter
+	@Setter
+	protected String content;
 
-    public ArrayList<Element> getChildren() {
-        return children;
-    }
+	public Element(String tag) {
+		this.tag = tag;
+		this.children = new ArrayList<Element>();
+		this.attributes = new HashMap<String, String>();
+		this.style = new HashMap<String, String>();
+		this.content = null;
+	}
 
-    public void appendChild(Element child) {
-        children.add(child);
-    }
+	public Element(Element e) {
+		this.tag = e.tag;
+		this.attributes = new HashMap<String, String>(e.attributes);
+		this.style = new HashMap<String, String>(e.style);
+		this.children = e.copyChildren();
+	}
 
-    public HashMap<String, String> getAttributes() {
-        return attributes;
-    }
+	protected ArrayList<Element> copyChildren() {
+		ArrayList<Element> childrenCopy = new ArrayList<Element>();
+		for (Element child : children) {
+			childrenCopy.add(new Element(child));
+		}
+		return childrenCopy;
+	}
 
-    public String getAttribute(String key) {
-        assert key != "style";
-        return attributes.get(key);
-    }
+	public void appendChild(Element child) {
+		children.add(child);
+	}
 
-    public void setAttribute(String key, String value) {
-        assert key != "style";
-        attributes.put(key, value);
-    }
+	public String getAttribute(String key) {
+		assert key != "style";
+		return attributes.get(key);
+	}
 
-    public void setStyle(String key, String value) {
-        style.put(key, value);
-    }
+	public void setAttribute(String key, String value) {
+		assert key != "style";
+		attributes.put(key, value);
+	}
 
-    public String getStyle(String key) {
-        return style.get(key);
-    }
+	public void setStyle(String key, String value) {
+		style.put(key, value);
+	}
 
-    public HashMap<String, String> getStyle() {
-        return style;
-    }
+	public String getStyle(String key) {
+		return style.get(key);
+	}
 
-    private String colorToStr(Color c) {
-        return c == null ? "none" : "#" + c.toHex();
-    }
+	private String colorToStr(Color c) {
+		return c == null ? "none" : "#" + c.toHex();
+	}
 
-    public String toStyleStr() {
-        StringBuilder sb = new StringBuilder();
-        for(String key : style.keySet()) {
-            String value = style.get(key);
-            sb.append(" ").append(key).append(":").append(value).append(";");
-        }
-        if(sb.length() == 0) {
-            return "";
-        }
-        return sb.substring(1);
-    }
+	public String toStyleStr() {
+		StringBuilder sb = new StringBuilder();
+		for (String key : style.keySet()) {
+			String value = style.get(key);
+			sb.append(" ").append(key).append(":").append(value).append(";");
+		}
+		if (sb.length() == 0) {
+			return "";
+		}
+		return sb.substring(1);
+	}
 
-    private void addIndentation(StringBuilder sb, int level) {
-        for(int i = 0; i < level; i++) {
-            sb.append("\t");
-        }
-    }
+	private void addIndentation(StringBuilder sb, int level) {
+		for (int i = 0; i < level; i++) {
+			sb.append("\t");
+		}
+	}
 
-    public void buildString(StringBuilder sb, int level) {
-        addIndentation(sb, level);
-        sb.append("<").append(tag);
-        for(String key : attributes.keySet()) {
-            String value = attributes.get(key);
-            sb.append(" ");
-            sb.append(key).append("=").append('"').append(value).append('"');
-        }
-        if(style.size() > 0) {
-            sb.append(" style=\"").append(toStyleStr()).append('"');
-        }
-        if(!transform.isIdentity()) {
-            sb.append(" transform=\"").append(transform.toSvgTransform()).append('"');
-        }
-        sb.append(">");
-        if(content != null) {
-            sb.append(content);
-        }
-        for(Element child : children) {
-            sb.append("\n");
-            child.buildString(sb, level + 1);
-        }
-        sb.append("\n");
-        addIndentation(sb, level);
-        sb.append("</").append(tag).append(">");
-    }
+	public void buildString(StringBuilder sb, int level) {
+		addIndentation(sb, level);
+		sb.append("<").append(tag);
+		for (String key : attributes.keySet()) {
+			String value = attributes.get(key);
+			sb.append(" ");
+			sb.append(key).append("=").append('"').append(value).append('"');
+		}
+		if (style.size() > 0) {
+			sb.append(" style=\"").append(toStyleStr()).append('"');
+		}
+		if (!transform.isIdentity()) {
+			sb.append(" transform=\"").append(transform.toSvgTransform()).append('"');
+		}
+		sb.append(">");
+		if (content != null) {
+			sb.append(content);
+		}
+		for (Element child : children) {
+			sb.append("\n");
+			child.buildString(sb, level + 1);
+		}
+		sb.append("\n");
+		addIndentation(sb, level);
+		sb.append("</").append(tag).append(">");
+	}
 
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        buildString(sb, 0);
-        return sb.toString();
-    }
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		buildString(sb, 0);
+		return sb.toString();
+	}
 
-    public void setFill(Color c) {
-        setAttribute("fill", colorToStr(c));
-    }
+	public void setFill(Color c) {
+		setAttribute("fill", colorToStr(c));
+	}
 
-    public void setStroke(Color c) {
-        setAttribute("stroke", colorToStr(c));
-    }
+	public void setStroke(Color c) {
+		setAttribute("stroke", colorToStr(c));
+	}
 
-    public void setStroke(int strokeWidth, int miterLimit, String lineJoin) {
-        setStyle("stroke-width", strokeWidth + "px");
-        setStyle("stroke-miterlimit", "" + miterLimit);
-        setStyle("stroke-linejoin", lineJoin);
-    }
+	public void setStroke(int strokeWidth, int miterLimit, String lineJoin) {
+		setStyle("stroke-width", strokeWidth + "px");
+		setStyle("stroke-miterlimit", "" + miterLimit);
+		setStyle("stroke-linejoin", lineJoin);
+	}
 
-    private Transform transform = new Transform();
-    public void transform(Transform t) {
-        transform.concatenate(t);
-    }
+	private Transform transform = new Transform();
 
-    public void setTransform(Transform t) {
-        if(t == null) {
-            transform.setToIdentity();
-        } else {
-            transform.setTransform(t);
-        }
-    }
+	public void transform(Transform t) {
+		transform.concatenate(t);
+	}
 
-    public Transform getTransform() {
-        return new Transform(transform);
-    }
+	public void setTransform(Transform t) {
+		if (t == null) {
+			transform.setToIdentity();
+		} else {
+			transform.setTransform(t);
+		}
+	}
 
-    public void rotate(double radians, double anchorx, double anchory) {
-        transform.rotate(radians, anchorx, anchory);
-    }
+	public Transform getTransform() {
+		return new Transform(transform);
+	}
 
-    public void rotate(double radians) {
-        transform.rotate(radians);
-    }
+	public void rotate(double radians, double anchorx, double anchory) {
+		transform.rotate(radians, anchorx, anchory);
+	}
 
-    public void translate(double x, double y) {
-        transform.translate(x, y);
-    }
+	public void rotate(double radians) {
+		transform.rotate(radians);
+	}
+
+	public void translate(double x, double y) {
+		transform.translate(x, y);
+	}
 
 }
